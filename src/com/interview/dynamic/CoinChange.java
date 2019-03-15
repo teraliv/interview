cea2f3c8cb01958ca744975ae9fb780fa1bd3f96
Alexander Terikov <teraliv@gmail.com>
2019-03-15 12:07:15 -0700

    leetcode Coin Change II

--- src/com/interview/dynamic/CoinChange.java
+++ src/com/interview/dynamic/CoinChange.java
@@ -5,8 +5,9 @@ package com.interview.dynamic;
  *
  *
  * https://leetcode.com/problems/coin-change/
+ * https://leetcode.com/problems/coin-change-2/
  *
- * Approach: DP
+ * Approach: DP - both problems
  * Time complexity: O(nm) where n = number of coins, m = amount
  * Space complexity: O(m)
  */
@@ -23,10 +24,8 @@ public class CoinChange {
         for (int i = 1; i < results.length; i++)
             results[i] = Integer.MAX_VALUE-1;
 
-        for (int i = 0; i < coins.length; i++) {
+        for (int coin : coins) {
             for (int j = 1; j <= amount; j++) {
-                int coin = coins[i];
-
                 if (j >= coin) {
                     if (results[j - coin] + 1 < results[j]) {
                         results[j] = 1 + results[j - coin];
@@ -40,13 +39,34 @@ public class CoinChange {
     }
 
 
+    /**
+     * Finds maximum number of unique ways to make up a given amount from available coins
+     *
+     * @return - number of unique ways or 0.
+     */
+    private int uniqueCombinationsChange(int amount, int[] coins) {
+        int[] results = new int[amount+1];
+        results[0] = 1;
+
+        for (int coin : coins) {
+            for (int j = 1; j < results.length; j++) {
+                if (j >= coin) {
+                    results[j] += results[j - coin];
+                }
+            }
+        }
+
+        return results[amount];
+    }
+
 
     public static void main(String[] args) {
         CoinChange cc = new CoinChange();
 
-        int[] coins = {300};
-        int amount = 100;
+        int[] coins = {1,2,5};
+        int amount = 5;
 
         System.out.println( cc.coinChange(coins, amount) );
+        System.out.println( cc.uniqueCombinationsChange(amount, coins));
     }
 }
