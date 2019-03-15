ddb23b5d8459ed6c7979add0b0b29f710482c27d
Alexander Terikov <teraliv@gmail.com>
2019-03-14 21:52:15 -0700

    leetcode Coin Change Problem

---
+++ src/com/interview/dynamic/CoinChange.java
@@ -0,0 +1,50 @@
+package com.interview.dynamic;
+
+/**
+ * 322. Coin Change
+ *
+ * https://leetcode.com/problems/coin-change/
+ *
+ *
+ * Approach: DP
+ */
+public class CoinChange {
+
+    /**
+     * Finds fewest number of coins that makes up the amount from given coins.
+     *
+     * @return - number of fewest coins or -1.
+     */
+    private int coinChange(int[] coins, int amount) {
+        int[] results = new int[amount + 1];
+
+        for (int i = 1; i < results.length; i++)
+            results[i] = Integer.MAX_VALUE-1;
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = 1; j <= amount; j++) {
+                int coin = coins[i];
+
+                if (j >= coin) {
+                    if (results[j - coin] + 1 < results[j]) {
+                        results[j] = 1 + results[j - coin];
+                    }
+                }
+            }
+
+        }
+
+        return (results[amount] != Integer.MAX_VALUE - 1) ? results[amount] : -1;
+    }
+
+
+
+    public static void main(String[] args) {
+        CoinChange cc = new CoinChange();
+
+        int[] coins = {300};
+        int amount = 100;
+
+        System.out.println( cc.coinChange(coins, amount) );
+    }
+}
